3745e5db54a2eed6ad86b8b58f08b1de8bdd89ab
gutsserrano <gutsserrano@gmail.com>
2023-08-19 02:09:02 -0300

    exercicio finalizado

--- aula4_18_agosto/exercicio_sequencial_augusto.c
+++ aula4_18_agosto/exercicio_sequencial_augusto.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-#define MAX 25
+#define MAX 1000000
 #define TRUE 1
 #define FALSE 0
 
@@ -14,12 +14,11 @@ void gera_vet(int vetor[]);
 void gera_kindex(int vetor[], struct sequencial_index tab[], int tamanho, int intervalo);
 int busca(int vetor[], struct sequencial_index tab[], int key, int intervalo);
 
-
 int main(int argc, char const *argv[]){
     int vetor[MAX];
-    int intervalo = 5;
-    int key = 124;
-    struct sequencial_index kindex[MAX];
+    int intervalo = 1000;
+    int key = 178354;
+    struct sequencial_index kindex[MAX / intervalo + 1];
     int achou;
     int posi;
 
@@ -33,32 +32,30 @@ int main(int argc, char const *argv[]){
         printf("\n[%d]: %d\n", posi, vetor[posi]);
     }else{
         printf("\nA chave %d nao existe no vetor!\n", key);
-
     }
     
     return 0;
 }
 
 void gera_vet(int vetor[]){
-    //srand( (unsigned) time(NULL) );
+    srand( (unsigned) time(NULL) );
     vetor[0] = 10;
     
     for(int i = 1; i < MAX; i++){
         vetor[i] = vetor[i - 1] + (rand() % 10) + 1;
     }
-
-    for(int i = 0; i<MAX; i++){
-        printf("[%d] = %d\n", i, vetor[i]);
-    }
 }
 
 void gera_kindex(int vetor[], struct sequencial_index tab[], int tamanho, int intervalo){
+    int i = 0;
 
-    for(int i = 0; i<=intervalo; i++){
-        tab[i].pos = i*intervalo;
-        tab[i].value = vetor[i*intervalo];
+    for(i = 0; i < tamanho / intervalo; i++){
+        tab[i].pos = i * intervalo;
+        tab[i].value = vetor[i * intervalo];
     }
     
+    tab[i].pos = tamanho - 1;
+    tab[i].value = vetor[tamanho - 1];
 }
 
 int busca(int vetor[], struct sequencial_index tab[], int key, int intervalo){
@@ -67,7 +64,7 @@ int busca(int vetor[], struct sequencial_index tab[], int key, int intervalo){
     int i;
     int j;
 
-    for(i = 1; i < MAX/intervalo && achou == -1 && ntem; i++){
+    for(i = 1; i <= MAX / intervalo && achou == -1 && ntem; i++){
         if(key <= tab[i].value){
             for(j = tab[i-1].pos; j <= tab[i].pos && achou == -1; j++){
                 if(key == vetor[j]){
@@ -79,13 +76,6 @@ int busca(int vetor[], struct sequencial_index tab[], int key, int intervalo){
             }
         }
     }
-    for(int k = 0; k<MAX/intervalo;k++){
-    printf("pos %d ", tab[k].pos);
-    printf("value %d\n", tab[k].value);
-        
-    }
 
     return achou;
 }
-
-
